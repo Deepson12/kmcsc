741ad0b265cd41d27590f8d2f85ac42974352399
Obscure-Git <dhakalkritish7@gmail.com>
2022-11-20 12:20:18 +0545

    fixed animations in waves, etc.

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -9,4 +9,4 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));   
+hiddenElements.forEach((el) => observer.observe(el));
